2962867279592b64b37b6103746d450c1fc51413
Your Name <you@example.com>
2025-05-22 01:36:14 +0530

    refactor: streamline Firebase Admin SDK initialization and remove redundant checks

--- src/lib/firebase-admin.js
+++ src/lib/firebase-admin.js
@@ -1,18 +1,6 @@
-import admin from "firebase-admin";
-import fs from "fs";
+const admin = require('firebase-admin');
+const serviceAccountPath = process.env.FIREBASE_ADMINSDK_PATH;
 
-if (!admin.apps.length) {
-  const serviceAccountPath = process.env.FIREBASE_ADMINSDK_PATH;
-
-  if (!serviceAccountPath) {
-    throw new Error("Missing FIREBASE_ADMINSDK_PATH env variable");
-  }
-
-  const serviceAccount = JSON.parse(fs.readFileSync(serviceAccountPath, "utf-8"));
-
-  admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-  });
-}
-
-export { admin };
+admin.initializeApp({
+  credential: admin.credential.cert(require(serviceAccountPath))
+});
