8744719a82fee77d57081085e2eb4317c91a3fed
Your Name <you@example.com>
2025-05-26 20:49:14 +0530

    fix: simplify Firebase app initialization by using service account directly

--- src/lib/firebase-admin.js
+++ src/lib/firebase-admin.js
@@ -1,17 +1,13 @@
 import { initializeApp, getApps, cert } from "firebase-admin/app";
 import { getMessaging } from "firebase-admin/messaging";
-
+import serviceAccount from "./serviceAccountKey.json"; // Adjust the path to your service account key
 // Initialize the app with your service account
 let adminApp;
 
 const privateKey = process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n")
 if (!getApps().length) {
   adminApp = initializeApp({
-    credential: cert({
-      projectId: process.env.FIREBASE_PROJECT_ID,
-      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-      privateKey: privateKey,
-    }),
+    credential: cert(serviceAccount),
   });
 } else {
   adminApp = getApps()[0];
