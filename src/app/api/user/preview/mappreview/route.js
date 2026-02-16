7fe3e62cfbe2a8d5f4aa044dcf289e352c4d7d18
Your Name <you@example.com>
2026-02-16 13:14:19 +0530

    refactor: simplify user fetching logic by removing pagination parameters

--- src/app/api/user/preview/mappreview/route.js
+++ src/app/api/user/preview/mappreview/route.js
@@ -8,22 +8,15 @@ export async function POST(req) {
   await connectDB();
 
   try {
-    const { page = 1 } = await req.json(); // get page from request body
-    const limit = 500;
-    const skip = (page - 1) * limit;
-
     const users = await UserModel.find()
       .select(
-        "-review -products -slug   -siteoffer -bio -mobileNumber -gstNumber -role -referralCode -visitCount -carauselImages -termsAccepted -admin -language -joiningDate -socialMediaLinks -subscriptionPlan -password -email -videoUrl"
+        "-review -products -slug  -siteoffer -bio -mobileNumber -gstNumber -role -referralCode -visitCount -carauselImages -termsAccepted -admin -language -joiningDate -socialMediaLinks -subscriptionPlan -password -email -videoUrl"
       )
       .populate("categories", "name")
-      .skip(skip)
-      .limit(limit)
       .lean();
 
     return NextResponse.json({
       success: true,
-      page,
       count: users.length,
       users,
     });
